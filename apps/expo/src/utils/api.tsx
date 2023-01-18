fd1ad4a50a095daa85c979dbebb975fd784a2369
Liz Ainslie <lizainslie16@gmail.com>
2023-01-18 01:08:27 -0700

    chore(init): configure boilerplate

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -1,5 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "@acme/api";
+import type { AppRouter } from "@clipbin/api";
 /**
  * A set of typesafe hooks for consuming your API.
  */
